83a07c4d5d07b8928ca0170ec5281dfc62824fe5
github username <github email address>
2019-04-02 17:30:13 -0400

    done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,21 @@
+class PigLatinizer
+    attr_reader :text
+
+    def initialize
+    end
+
+    def piglatinize(text)
+        new_text = ""
+        text.split(" ").each do |word|
+            first_vowel_position = word =~ /[aeiouAEIOU]/
+            if first_vowel_position == 0
+                new_text << "#{word}way "
+            else
+                vowel_index = word =~ /[aeiouAEIOU]/
+                consonants = word[0..vowel_index-1]
+                new_text << "#{word[vowel_index..]}#{consonants}ay "
+            end
+        end
+        new_text.rstrip
+    end
+end
