47df528a9ec3c4836ab97ebf6d993968cac12ac5
github username <github email address>
2019-04-02 17:42:35 -0400

    done. added y to vowels.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,12 +7,12 @@ class PigLatinizer
     def piglatinize(text)
         new_text = ""
         text.split(" ").each do |word|
-            first_vowel_position = word =~ /[aeiouAEIOU]/
+            first_vowel_position = word =~ /[aeiouyAEIOUY]/
             if first_vowel_position == 0
                 new_text << "#{word}way "
             else
-                vowel_index = word =~ /[aeiouAEIOU]/
-                consonants = word[0..vowel_index-1]
+                vowel_index = word =~ /[aeiouyAEIOUY]/
+                consonants = word[0..(vowel_index-1)]
                 new_text << "#{word[vowel_index..]}#{consonants}ay "
             end
         end
